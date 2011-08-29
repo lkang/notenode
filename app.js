c613b4e0e3a079d0a3eeaba57aa25c08123d4172
Larry Kang <larry@cotweet.com>
2011-08-28 22:57:22 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,75 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+
+var app = module.exports = express.createServer();
+io = require('socket.io').listen(app);
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Express',
+    myname: 'lkang'
+  });
+});
+
+
+io.sockets.on('connection', function (socket) {
+  // socket.emit('news', { hello: 'world turning' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+  socket.on('newnote', function(data) {
+    console.log('got a new note');
+    console.log(data);
+    
+    io.sockets.emit( 'update', { text: data.text, guid: data.guid } );
+  });
+  
+  socket.on('created', function(data) {
+    console.log('created a new note')
+    console.log(data);
+    io.sockets.emit( 'create', { text: data.text, guid: data.guid });
+  })
+  
+  socket.on('dragstop', function(data) {
+    console.log('got dragstop data');
+    console.log(data);
+    io.sockets.emit( 'dragstop', { guid: data.guid, top: data.top, left: data.left });
+  })
+  
+  socket.on('drag', function(data) {
+    console.log('got drag data');
+    console.log(data);
+    io.sockets.emit( 'drag', { guid: data.guid, top: data.top, left: data.left });
+  })
+});
+
+
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ public/javascripts/msg.js
@@ -0,0 +1,79 @@
+// function() {
+  
+  var socket = io.connect('http://192.168.1.107');
+  
+  global_id = 1;
+  function nextid() {
+    global_id = global_id + 1 + new Date().valueOf();
+    return global_id;
+  }
+  
+  socket.on('create', function(data) {
+    //create a new one. fill it with data. 
+    var newmsg = $('#proto_msg').children().first().clone();
+    var id = data.guid
+    
+    newmsg.draggable();
+    newmsg.attr('id', id );
+    newmsg.css('position', 'absolute');
+    $('#msgcanvas').append(newmsg);
+    
+    var msginput = $('textarea.msginput', newmsg);
+    var msgoutput = $('.msgarea', newmsg)
+    msginput.keyup( function( event ) {
+      if( event.keyCode == 13 ) {
+        text = msginput.val();
+        msginput.val('');
+        // var m = $('<p>'+text+'</p>');
+        // msgoutput.text( text );
+        socket.emit( 'newnote', { text: text, guid: id })
+      }
+    });
+    
+    newmsg.bind('dragstop', function(event) {
+      socket.emit( 'dragstop', { guid : id, left: newmsg.css('left'), top: newmsg.css('top')})
+    });
+    
+    // newmsg.bind('drag', function(event) {
+      // socket.emit( 'drag', { guid : id, left: newmsg.css('left'), top: newmsg.css('top')})
+    // })
+
+    socket.on('drag', function(data) {
+      if( data.guid != id )
+        return;
+      var tol = 10;
+      var left = parseInt( newmsg.css('left'), 10 );
+      var top  = parseInt( newmsg.css('top'), 10 );
+      var dleft = parseInt( data.left, 10 );
+      var dtop  = parseInt( data.top, 10);
+      if((dleft > left + tol) || (dleft < left - tol) )
+        newmsg.css('left', data.left );
+      if((dtop > top + tol) || (dtop < top - tol) )
+        newmsg.css('top', data.top);
+    })
+    
+        
+    socket.on('dragstop', function(data) {
+      if( data.guid != id )
+        return;
+      newmsg.css('left', data.left );
+      newmsg.css('top', data.top);
+    })
+    
+    socket.on('update', function(data) {
+      console.log( data );
+      if( data.guid != id )
+        return; 
+      // addit = $('<p>'+data.text+'</p>');
+      // msgoutput.append( addit );
+      msgoutput.text(data.text)
+    });
+  })
+  
+  $('body').dblclick( function(event) {
+    socket.emit( 'created', { text: "", guid: nextid()} );
+  })
+
+
+// }();
+  
