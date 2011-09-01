991db0807fd13fde3b75bbc2f9cf5d45e446a759
Larry Kang <larry@cotweet.com>
2011-08-31 20:10:29 -0700

    use env

--- server.js
+++ server.js
@@ -71,6 +71,6 @@ io.sockets.on('connection', function (socket) {
 });
 
 
-
-app.listen(80);
+port = process.env.PORT || 3000
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
